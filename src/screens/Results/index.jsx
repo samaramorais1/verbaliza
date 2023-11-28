0e7c90addf4eb4668c6ff2853bda9c957fa580a7
samaramorais1 <133446388+samaramorais1@users.noreply.github.com>
2023-11-27 22:18:32 -0300

    page final

--- src/screens/Results/index.jsx
+++ src/screens/Results/index.jsx
@@ -1,12 +1,28 @@
 import { View, Text, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
 
 export default function Results({ navigation }) {
   return (
-    <View>
-      <Text>Results</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text>Voltar p/ tela inicial</Text>
+  
+    <View  style={styles.container}>
+         <View style={styles.textCard}>
+        <Text style={styles.textinput}> Escreva seu texto aqui</Text>
+
+        </View>
+
+        <View style={styles.containerBaixo} >
+  
+        </View>
+
+        
+      
+      <TouchableOpacity style={styles.voltar} onPress={() => navigation.goBack()}>
+        <Text>Voltar</Text>
       </TouchableOpacity>
+
+
+      
+     
     </View>
   );
 }
---
+++ src/screens/Results/styles.js
@@ -0,0 +1,44 @@
+import {StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    container: {
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#FFFBD3',
+            },
+    textinput: {
+      fontSize: 15,
+      textAlign: 'center',
+    },
+    textCard: {
+     backgroundColor:'#F4D3FF',
+     justifyContent:'center',
+     height: '5%', 
+     width:'90%',
+     alignItems:'center',
+     marginTop: 30,  
+     borderRadius: 10,
+     
+    
+   
+    },
+    
+    containerBaixo: {
+    backgroundColor:'#F4D3FF',
+     justifyContent:'center',
+     height:'70%',
+     width:'90%',
+     marginTop: 30, 
+     alignItems:'center',
+     borderRadius: 30,
+    },
+    voltar:{
+        marginTop:50,
+        width:'65%',
+        height: 50,
+        backgroundColor:'#FFE4C4',
+        borderRadius: 50,
+        alignItems:'center',
+       borderWidth:2,
+       textAlign: 'center',
+    }
+})
